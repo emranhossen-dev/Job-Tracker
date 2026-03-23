bd5e71a0b0be9a6c66c41601bfd0dce20229d478
emran40989 <emran40989@gmail.com>
2026-03-24 05:23:22 +0600

    updated the logic of count

--- script.js
+++ script.js
@@ -1,79 +1,74 @@
-// lets play with the dom 
-// interview button
 const interviewbtns = document.getElementsByClassName("interview-action");
-for(const btn of interviewbtns){
-    btn.addEventListener("click", function(){
+for (const btn of interviewbtns) {
+    btn.addEventListener("click", function () {
         const card = btn.closest(".job-card");
         const status = card.querySelector(".status-btn");
+        
         status.innerText = "Interview";
-        status.classList.remove("btn-soft", "btn-warning");
+        status.classList.remove("btn-soft", "btn-warning", "rejected-done");
         status.classList.add("btn-success", "interview-done");
+        
         updateInterviewCount();
+        updateRejectCount();
     })
 }
 
-// update count
 function updateInterviewCount() {
     const completedInterviews = document.getElementsByClassName("interview-done").length;
     document.getElementById("interview-count").innerText = completedInterviews;
 }
 
-// reject button
 const rejectbtns = document.getElementsByClassName("rejected-action");
-for (const btn of rejectbtns){
-    btn.addEventListener("click", function(){
+for (const btn of rejectbtns) {
+    btn.addEventListener("click", function () {
         const card = btn.closest(".job-card");
         const status = card.querySelector(".status-btn");
-        status.innerText="Rejected";
-        status.classList.remove("btn-soft", "btn-success");
+        
+        status.innerText = "Rejected";
+        status.classList.remove("btn-soft", "btn-success", "interview-done");
         status.classList.add("btn-warning", "rejected-done")
+        
+        updateInterviewCount();
         updateRejectCount();
     })
 }
 
-// update count
 function updateRejectCount() {
     const rejected = document.getElementsByClassName("rejected-done").length;
     document.getElementById("reject-count").innerText = rejected;
 }
 
-
-// filter button
 const allBtn = document.getElementById("all-job");
 const interviewBtn = document.getElementById("interview-job");
 const rejectedBtn = document.getElementById("rejected-job");
-const cards = document.querySelectorAll(".job-card");
 
-// all button
 allBtn.addEventListener("click", function () {
-  for (const card of cards) {
-    card.style.display = "block";
-  }
+    const cards = document.querySelectorAll(".job-card");
+    for (const card of cards) {
+        card.style.display = "block";
+    }
 });
 
-// interview button
 interviewBtn.addEventListener("click", function () {
-  for (const card of cards) {
-    const status = card.querySelector(".status-btn").innerText;
-
-    if (status === "Interview") {
-      card.style.display = "block";
-    } else {
-      card.style.display = "none";
+    const cards = document.querySelectorAll(".job-card");
+    for (const card of cards) {
+        const status = card.querySelector(".status-btn").innerText;
+        if (status === "Interview") {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
     }
-  }
 });
 
-// reject button
 rejectedBtn.addEventListener("click", function () {
-  for (const card of cards) {
-    const status = card.querySelector(".status-btn").innerText;
-
-    if (status === "Rejected") {
-      card.style.display = "block";
-      
-    } else {
-      card.style.display = "none";
+    const cards = document.querySelectorAll(".job-card");
+    for (const card of cards) {
+        const status = card.querySelector(".status-btn").innerText;
+        if (status === "Rejected") {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
     }
-  }
 });
